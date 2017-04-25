415598756d4a7b424906ee9a71834925c6da2c0d
dicksonmuli <dicksonmully6@gmail.com>
2017-04-25 12:31:23 +0300

    add task route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,5 +13,17 @@ public class App {
 			model.put("template", "template/index.vtl");
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
+
+		// task route
+		post("/tasks", (request, response) -> {
+		 Map<String, Object> model = new HashMap<String, Object>();
+
+		 String description = request.queryParams("description");
+		 Task newTask = new Task(description);
+		 request.session().attribute("task", newTask);
+
+		 model.put("template", "templates/success.vtl");
+		 return new ModelAndView(model, layout);
+	 }, new VelocityTemplateEngine());
 	}
 }
