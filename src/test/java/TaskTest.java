4a107ea3cdf93faf0f4f2b5c4bbd8e406a9c792a
dicksonmuli <dicksonmully6@gmail.com>
2017-04-25 11:45:20 +0300

    first test- confirm if task objects can be instantiated successfully

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TaskTest {
-	
+	@Task
+	public void Task_instantiatesCorrectly_true() {
+		Task myTask = new Task("Mow the lawn");
+		assertEquals(true, myTask instanceof Tast);
+	}
 }
