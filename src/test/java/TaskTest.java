d77e8f48d50f3eccfcdb98984ea857183cdd5b42
dicksonmuli <dicksonmully6@gmail.com>
2017-04-25 11:54:06 +0300

    assigning each task a description and then retrieve it

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -2,9 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TaskTest {
-	@Task
+	// creating an instance of Task successfully
+	@Test
 	public void Task_instantiatesCorrectly_true() {
 		Task myTask = new Task("Mow the lawn");
-		assertEquals(true, myTask instanceof Tast);
+		assertEquals(true, myTask instanceof Task);
 	}
+	// assigning each task a description and then retrieve it
+	@Test
+	public void Task_instantiatesWithDescription_String() {
+		Task myTask = new Task("Mow the lawn");
+		assertEquals("Mow the lawn", myTask.getDescription());
+	}
+
 }
