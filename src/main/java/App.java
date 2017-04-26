6e609b2ff991d9a3a6f4bb0a3a5c8e628f765153
dicksonmuli <dicksonmully6@gmail.com>
2017-04-26 16:42:40 +0300

    refactoring the app and adding routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,26 +12,48 @@ public class App {
 		// root route
 		get("/", (request, response) -> {
 			Map<String, Object> model = new HashMap<String, Object>();
-			model.put("tasks", request.session().attribute("tasks"));
+			// model.put("tasks", request.session().attribute("tasks"));
 			model.put("template", "templates/index.vtl");
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
+		// new tast
+		get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+		// task routes
+		get("/tasks", (request, response) -> {
+		 Map<String, Object> model = new HashMap<String, Object>();
+		 model.put("tasks", Task.all());
+		 model.put("template", "templates/tasks.vtl");
+		 return new ModelAndView(model, layout);
+	 }, new VelocityTemplateEngine());
 
-		// task route
 		post("/tasks", (request, response) -> {
 		 Map<String, Object> model = new HashMap<String, Object>();
-		 ArrayList<Task> tasks = request.session().attribute("tasks");
-		 if(tasks == null) {
-			 tasks = new ArrayList<Task>();
-			 request.session().attribute("tasks", tasks);
-		 }
+		//  (using sessions but we dont need it anymore since we are storing tasks )
+		//  ArrayList<Task> tasks = request.session().attribute("tasks");
+		//  if(tasks == null) {
+		// 	 tasks = new ArrayList<Task>();
+		// 	 request.session().attribute("tasks", tasks);
+		//  }
 
 		 String description = request.queryParams("description");
 		 Task newTask = new Task(description);
-		 tasks.add(newTask);
+		//  tasks.add(newTask);
 
 		 model.put("template", "templates/success.vtl");
 		 return new ModelAndView(model, layout);
 	 }, new VelocityTemplateEngine());
+
+	//  dynamic route for every task
+	get("/tasks/:id", (request, response) -> {
+  HashMap<String, Object> model = new HashMap<String, Object>();
+  Task task = Task.find(Integer.parseInt(request.params(":id")));
+  model.put("task", task);
+  model.put("template", "templates/task.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
 	}
 }
