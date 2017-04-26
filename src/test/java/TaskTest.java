59445d165247dbc3d52b223c2686c64789fe9168
dicksonmuli <dicksonmully6@gmail.com>
2017-04-26 08:07:44 +0300

    add date and instaces to a list

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -13,6 +14,26 @@ public class TaskTest {
 	public void Task_instantiatesWithDescription_String() {
 		Task myTask = new Task("Mow the lawn");
 		assertEquals("Mow the lawn", myTask.getDescription());
+	}
+	//equate to false if a task if not completed
+	@Test
+	public void isCompleted_isFalseAfterInstantiation_false() {
+		Task myTask = new Task("Mow the lawn");
+		assertFalse(myTask.isCompleted());
+	}
+	//instanciate with time
+	@Test
+	public void getCreatedAt_instantiatesWithCurrentTime_today() {
+		Task myTask = new Task("Mow the lawn");
+		assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+	}
+	//returns all instances of task
+	@Test
+	public void all_returnsAllInstancesOfTask_true() {
+		Task firtTask = new Task("Mow the lawn");
+		Task secondTask = new Task("Buy groceries");
+		assertEquals(true, Task.all().contains(firtTask));
+		assertEquals(true, Task.all().contains(secondTask));
 	}
 
 }
