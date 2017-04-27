2bf590310d7dc5b7753babf1686a2fe292e155fb
dicksonmuli <dicksonmully6@gmail.com>
2017-04-27 12:35:35 +0300

    if a port is set for the app, use it else continue with 4567

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,16 @@ public class App {
 	public static void main(String[] args) {
 		staticFileLocation("/public");
 		String layout = "templates/layout.vtl";
+		// if a port is set for the app, use it else continue with 4567
+		ProcessBuilder process = new ProcessBuilder();
+		Integer port;
+		if (process.environment().get("PORT") !=null) {
+			port = Integer.parseInt(process.environment().get("PORT"));
+		}else {
+			port = 4567;
+		}
+		setPort(port);
+		
 		// root route
 		get("/", (request, response) -> {
 			Map<String, Object> model = new HashMap<String, Object>();
