21f3f42b2274d734caaeae02b9910722506bb64d
dicksonmuli <dicksonmully6@gmail.com>
2017-05-02 17:00:18 +0300

    fixing errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,7 +16,7 @@ public class App {
 		if (process.environment().get("PORT") !=null) {
 			port = Integer.parseInt(process.environment().get("PORT"));
 		}else {
-			port = 4567;
+			port = 5432;
 		}
 		setPort(port);
 
