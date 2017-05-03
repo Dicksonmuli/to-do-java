9e25aecff4e0c0294ede53ec3a28582687f7d02a
dicksonmuli <dicksonmully6@gmail.com>
2017-05-03 08:16:58 +0300

    connects to port 4567 if no port is provided by heroku

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -4,21 +4,9 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 public class CategoryTest {
-  @Before
-     public void setUp() {
-       DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test","dickson", "dickson");
-     }
-
-     @After
-     public void tearDown() {
-       try(Connection con = DB.sql2o.open()) {
-         String deleteTasksQuery = "DELETE FROM tasks *;";
-         String deleteCategoriesQuery = "DELETE FROM categories *;";
-         con.createQuery(deleteTasksQuery).executeUpdate();
-         con.createQuery(deleteCategoriesQuery).executeUpdate();
-       }
-     }
-
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+  
   @Test
   public void category_instantiatesCorrectly_true() {
     Category testCategory = new Category("Home");
