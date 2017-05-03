6214b349136b3ba55876eced8feed0f308dd8552
dicksonmuli <dicksonmully6@gmail.com>
2017-05-03 09:28:40 +0300

    add db connection

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,6 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", "dickson", "dickson");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", "dickson", "dickson");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-235-120-39.compute-1.amazonaws.com:5432/d5puid2u1ldeqm", "lbxmmgtzlerudc", "8a79283ee338552c5aa41d3876df680467652a19ccbf7faf541177029f7bdde1");
 }
